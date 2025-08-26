3f95d2e7bd755133cb6535f79fcaedf8119161e7
WaSender23 <159567403+WaSender23@users.noreply.github.com>
2025-08-26 13:25:22 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-//http://vite.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/licen-anual/'
+  base: '/meu-primeiro-deploy/'
 })
